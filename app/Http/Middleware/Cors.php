41a342f2ad21adb865e552b9a6d2fd9d406e52b1
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2023-11-17 16:07:30 +0530

    new changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,6 +10,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->header('Access-Control-Allow-Origin', 'http://13.235.245.17')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
